075a59231e155224968a2d67a18cdc0bff858fc8
eugenelee-nm2207 <125366103+eugenelee-nm2207@users.noreply.github.com>
2023-03-31 18:23:44 +0800

    Week 11 Update
    Most changes to data story + updated Week 11 diary entry

--- appscripts/main.js
+++ appscripts/main.js
@@ -3,32 +3,31 @@ const navMenu = document.querySelector(".nav");
 const navOverlay = document.querySelector(".nav-overlay");
 const navButton = document.querySelector(".navButton");
 
+// allows navigation menu to appear when button is clicked
 navButton.addEventListener("click", () => {
     navMenu.classList.add("nav-open");
     navOverlay.classList.add("nav-overlay-open");
 });
 
+// allows navigation menu to disappear when any other area of the screen is clicked
 navOverlay.addEventListener("click", () => {
     navMenu.classList.remove("nav-open");
     navOverlay.classList.remove("nav-overlay-open");
 });
 
-// trying out theme changer
-  let counter = 0;
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("themeLogo").onclick = themeChanger;
-});
+// Theme changer
+  let counterThemeChanger = 0; //counter helps me to cycle through the different themes
 
 function themeChanger() {
-  counter++;
-  if (counter === 1) {
+  counterThemeChanger++;
+  if (counterThemeChanger === 1) {
     alert("Changed theme!");
     document.getElementById("themeLogo").src="resources/twiceLogo.svg.png";
     document.getElementById("themePicture").src="resources/twice-background.jpg";
     document.body.style.backgroundColor = "rgb(249, 197, 150)";
     navBarColor = document.getElementById("topNavBar");
   } else {
-    counter = counter-2;
+    counterThemeChanger = counterThemeChanger-2;
     alert("Changed theme!");
     document.getElementById("themeLogo").src="resources/BlackpinkLogo.svg.png";
     document.getElementById("themePicture").src="resources/blackpink-background.jpg";
@@ -74,6 +73,7 @@ const introFansData = {
       }
   });
 
+  // showing a different fact based on whether male, female or others was clicked
   function GenderRatio1() {
     document.getElementById("ratioFact").innerHTML = "According to Lafan (2020), you would be among " +"<span class='highlight'>" + "26.7%" + "</span>" + " of Kpop fans.";
   };
@@ -96,11 +96,11 @@ const introFansData = {
   };*/
  
 
-//source = https://codepen.io/plotly/pen/EVrRxR
+//making a map chart of most popular Kpop groups in several countries. source = https://codepen.io/plotly/pen/EVrRxR
 Plotly.plot('googleSearchCountry', [{
   type: 'choropleth',
-  locations: ['USA','AUS','GBR','KOR','JPN','IDN'],
-  z: [0.4, 0.2, 0.2, 0.4, 0.2, 0.6]
+  locations: ['USA','AUS','GBR','KOR','JPN','IDN','CHN'],
+  z: [0.4, 0.2, 0.2, 0.4, 0.2, 0.6, 0.6]
 }], {
   geo: {
     resolution: 60,
@@ -112,12 +112,203 @@ Plotly.plot('googleSearchCountry', [{
     }
   },width:825, height:525,
 })
-const KoreaTop = ["BTS", "Apink", "Girl's Generation", "EXO", "GOT7"];
-function printTopGroups () {
+
+// -- click to find out the top groups of each country --
+
+//listing the datasets
+const AusTop = ["BTS (Male)", "Girl's Generation (Female)", "GOT7 (Male)", "SHINee (Male)", "iKON (Male)"];
+const UKTop = ["BTS (Male)", "BIGBANG (Male)", "GOT7 (Male)", "Girl's Generation (Female)", "2PM (Male)"];
+const USTop = ["Girl's Generation (Female)","BTS (Male)", "BIGBANG (Male)", "Blackpink (Female)",  "SHINee (Male)"];
+const ChinaTop = ["Girl's Generation (Female)","T-ara (Female)", "BIGBANG (Male)",  "EXO (Male)", "AOA (Female)"];
+const IndoTop = ["EXO (Male)", "Red Velvet (Female)",  "SISTAR (Female)", "GOT7 (Male)", "Girl's Generation (Female)"];
+const JapanTop = ["JYJ (Male)","Girl's Generation (Female)", "CNBLUE (Male)",  "2PM (Male)", "BTS (Male)"];
+const PhilTop = ["Girl's Generation (Female)","BTS (Male)", "EXO (Male)",  "GOT7 (Male)", "Red Velvet (Female)"];
+const KoreaTop = ["BTS (Male)", "Apink (Female)", "Girl's Generation (Female)", "EXO (Male)", "GOT7 (Male)"];
+
+// link buttons to printing function using event listener 
+document.getElementById("buttonAus").addEventListener("click", printTopGroups);
+document.getElementById("buttonUK").addEventListener("click", printTopGroups);
+document.getElementById("buttonUS").addEventListener("click", printTopGroups);
+document.getElementById("buttonChina").addEventListener("click", printTopGroups);
+document.getElementById("buttonIndo").addEventListener("click", printTopGroups);
+document.getElementById("buttonJap").addEventListener("click", printTopGroups);
+document.getElementById("buttonPhil").addEventListener("click", printTopGroups);
+document.getElementById("buttonKor").addEventListener("click", printTopGroups);
+
+// using for loop + if/else statement to print out lists of top kpop groups in each country in each array
+function printTopGroups(event) {
   let text = "";
-  var i = 0; 
-  for (i = 0; i < KoreaTop.length; i++) {
-    text += KoreaTop[i] + "<br>";
+  const buttonId = event.target.id;
+  for (let i = 0; i < 5; i++) {
+    if (buttonId === "buttonAus") { text += AusTop[i] + "<br>";
+  } else if (buttonId === "buttonUK") {text += UKTop[i] + "<br>";
+  } else if (buttonId === "buttonUS") {text += USTop[i] + "<br>";
+  } else if (buttonId === "buttonChina") {text += ChinaTop[i] + "<br>";
+  } else if (buttonId === "buttonIndo") {text += IndoTop[i] + "<br>";
+  } else if (buttonId === "buttonJap") {text += JapanTop[i] + "<br>";
+  } else if (buttonId === "buttonPhil") {text += PhilTop[i] + "<br>";
+    } else {
+      text += KoreaTop[i] + "<br>";
+    }
   }
   document.getElementById("topGroupList").innerHTML = text;
 }
+
+// -- Physical Album Sales Bar Charts --
+
+// Make a request for the CSV file
+const data = fetch("resources/data/best-selling-physical-by-album-sorted.csv")
+.then(function (response){
+    return response.text();
+}) 
+.then(function(data){
+    const table = [];
+    const rows = data.split("\r\n");
+
+    rows.forEach((r,index) => {
+        const item = r.split(",");
+        table.push(item);
+    });
+    console.log(table);
+
+    const albumLabel = [];
+    const groupLabel = [];
+    const genderLabel = [];
+    const salesData = [];
+
+    //using for loop to add elements to data arrays created above
+    for(j=0;j<table.length;j++)
+    {
+        //console.log(table[j][3]);
+        if(table[j][4]>=-0)
+        {
+          albumLabel.push(table[j][3]);  
+          groupLabel.push(table[j][2]);
+          genderLabel.push(table[j][1]);
+          salesData.push(table[j][4]);
+        }
+    };
+
+    //using for loop to add colors to bars in bar chart below without manual input. red for female, blue for male
+    const albumColors = [];
+    function albumLabelColor() {
+      var i = 0; 
+      for (i = 0; i < 11; i++) {
+        albumColors.push("red");
+      };
+      for (i = 11; i < 54; i++) {
+        albumColors.push("blue");
+      };
+    };
+    albumLabelColor();
+
+    //dataObj for the top album sales chart
+    const dataObj = {
+       labels: albumLabel,
+        datasets: [
+            {
+              label: "Sales",
+              data: salesData,
+                borderWidth: 2,
+                backgroundColor: albumColors,
+                //borderColor: "hsla(0,100%,50%,1)"
+            }
+        ]
+    };
+
+    //creating the bar chart
+    const albumChart = new Chart("best-selling-album",
+    {
+        type: "bar",
+        data: dataObj,
+        options: { 
+            maintainAspectRatio: false,
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+                text: ['Albums with the Top Sales in South Korea'],
+                fontFamily: "TrebuchetMS",
+                fontSize: 24,
+                fontColor: 'rgb(0, 0, 0)',
+            },
+            scales : {
+                yAxes:[{
+                    ticks:{
+                        beginAtZero: true,
+                        suggestedMin: 0,
+                        suggestedMax: 5100000,
+                    }
+                }],
+                xAxes:[{
+                  ticks:{
+                      display:false,
+                  },
+              }],
+            },
+        }
+    }); 
+
+// -- chart with only 1 album per group
+
+// datasets for graph with only 1 album per group:
+const albumLabel2 = ["THE REVE FESTIVAL 2022 - BIRTHDAY","ANTIFRAGILE","CHESHIRE","BETWEEN 1&2","OMG","AFTER LIKE","GIRLS - THE 2ND MINI ALBUM","BORN PINK","MAP OF THE SOUL: 7","4TH ALBUM `FACE THE SUN`","MAXIDENT","THE NAME CHAPTER: TEMPTATION","STICKER - THE 3RD ALBUM","맛 (HOT SAUCE) - THE 1ST ALBUM","RESONANCE PT. 1","MANIFESTO: DAY 1","DON'T MESS UP MY TEMPO"];
+const salesData2 = [1014751,1026011,1056316,1130956,1243200,1667348,1823499,2679375,5022205,3461702,3256303,2634872,2434408,2168436,1670800,1563356,1452030];
+
+// to edit colors in new chart
+const albumColors2 = [];
+function albumLabelColor2() {
+  var i = 0; 
+  for (i = 0; i < 8; i++) {
+    albumColors2.push("red");
+  };
+  for (i = 8; i < 17; i++) {
+    albumColors2.push("blue");
+  };
+};
+albumLabelColor2;
+
+// dataObj2 for graph with only 1 album per group:
+const dataObj2 = {
+  labels: albumLabel2,
+   datasets: [
+       {
+         label: "Sales",
+         data: salesData2,
+           borderWidth: 2,
+           backgroundColor: albumColors2,
+           //borderColor: "hsla(0,100%,50%,1)"
+       }
+   ]
+};
+counterAlbumChanger = 0
+function updateAlbumChart(){
+  counterAlbumChanger++;
+  if (counterAlbumChanger === 1) {
+  albumChart.data.labels = albumLabel2;
+  albumChart.data.datasets[0] = {
+  label: "Sales",
+  data: salesData2,
+  backgroundColor: ["red","red","red","red","red","red","red","blue","blue","blue","blue","blue","blue","blue","blue","blue","blue"],
+}
+albumChart.update(); 
+document.getElementById("buttonPhysChange").innerHTML = "Toggle View to All Albums";
+  } else {
+    counterAlbumChanger = counterAlbumChanger-2;
+    albumChart.data.labels = albumLabel;
+  albumChart.data.datasets[0] = {
+  label: "Sales",
+  data: salesData,
+  backgroundColor: albumColors,}
+  albumChart.update(); 
+  document.getElementById("buttonPhysChange").innerHTML = "Toggle View to Unqiue Albums Only";
+
+  }
+};
+
+document.getElementById("buttonPhysChange").addEventListener("click",updateAlbumChart);
+
+})
+
+
--- diary/appscripts/main.js
+++ diary/appscripts/main.js
@@ -3,18 +3,20 @@ const navMenu = document.querySelector(".nav");
 const navOverlay = document.querySelector(".nav-overlay");
 const navButton = document.querySelector(".navButton");
 
+// allows navigation menu to appear when button is clicked
 navButton.addEventListener("click", () => {
     navMenu.classList.add("nav-open");
     navOverlay.classList.add("nav-overlay-open");
 });
 
+// allows navigation menu to disappear when any other area of the screen is clicked
 navOverlay.addEventListener("click", () => {
     navMenu.classList.remove("nav-open");
     navOverlay.classList.remove("nav-overlay-open");
 });
 
-// trying out theme changer
-let counter = 0;
+// Theme changer
+let counter = 0; //counter helps me to cycle through the different themes
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelector("themeLogo").onclick = themeChanger;
 });
